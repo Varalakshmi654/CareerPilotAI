a2ec7566e9b6f27399a212ee1edddd66bf14c86c
Varalakshmi654 <eathavaralakshmi@gmail.com>
2026-06-17 20:07:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("🚀 CareerPilot AI")
+
+st.header("AI-Powered Placement Success Predictor and Skill Gap Analyzer")
+
+st.success("Website is working successfully!")
